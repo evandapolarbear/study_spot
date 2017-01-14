59f69d9d5737bbc158f0f5c095e261c1f1c77d98
BendersBender <ev.l.waits@gmail.com>
2017-01-13 16:35:03 -0800

    page divisions fixed

--- frontend/components/main/main.jsx
+++ frontend/components/main/main.jsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router';
 
 import GreetingContainer from '../greeting/greeting_container';
 import SpotMap from '../map/map';
-import SpotShowContainer from '../spot_show/spot_show_container';
 
 const Main = ({ spots, updateFilter, requestSpots, setCurrentSpot, children }) => (
   <div className='main'>
