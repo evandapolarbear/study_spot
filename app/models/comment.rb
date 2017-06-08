9e8070ffbf8b878d2f26e1ee1634e089bdb3b94d
BendersBender <ev.l.waits@gmail.com>
2017-06-08 16:45:17 -0700

    fix error comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,6 @@ class Comment < ActiveRecord::Base
   validates :user_id, :spot_id, :body, presence: true
   validates :body, length: {minimum: 4}
 
-  belongs_to :spot, :user
+  belongs_to :spot
+  belongs_to :user
 end
